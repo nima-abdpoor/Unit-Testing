fadd6abe18a88ecb0609185f78f75a35dee298f7
nimaabdpoor <nimaabdi328@yahoo.com>
2020-08-18 17:15:58 +0430

    assertion test for Equals method in Money.java

--- src/com/company/MoneyTest.java
+++ src/com/company/MoneyTest.java
@@ -2,14 +2,19 @@ package com.company;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 
 public class MoneyTest {
     @Test
     public void ConstructorShouldSetAmountAndCurrency() {
         Money money =new Money(10, "USD");
+        Money money1 = new Money(10,"USD");
+        Money money2 = new Money(20,"IR");
         assertEquals(10, money.getAmount());
         assertEquals("USD",money.getCurrency());
+        assertTrue(money.equals(money));
+        assertTrue(money1.equals(money));
+        assertFalse(money2.equals(money));
     }
 }
