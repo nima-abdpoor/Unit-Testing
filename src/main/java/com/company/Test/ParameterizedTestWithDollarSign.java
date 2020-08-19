c4101ce6c2bde75480449d201d32faa9f2c28851
nimaabdpoor <nimaabdi328@yahoo.com>
2020-08-19 17:45:49 +0430

    Parameterized Test With Dollar Sign($)

---
+++ src/main/java/com/company/Test/ParameterizedTestWithDollarSign.java
@@ -0,0 +1,28 @@
+package com.company.Test;
+
+import com.company.Money;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junitparams.JUnitParamsRunner.$;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnitParamsRunner.class)
+public class ParameterizedTestWithDollarSign {
+    private static final Object[] getMoney(){
+        return $(
+                $(10,"USD"),
+                $(20,"EUD")
+        );
+    }
+    @Test
+    @Parameters(method = "getMoney")
+    public void ConstructorShouldSetAmountAndCurrency(int amount, String currency){
+        Money money =new Money(amount,currency);
+        assertEquals(amount, money.getAmount());
+        assertEquals(currency, money.getCurrency());
+    }
+
+}
